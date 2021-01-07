9bd135eff33265eff0c21811ff421bf25f5ae567
Adeline Graciani <50612536+8deline@users.noreply.github.com>
2021-01-08 00:16:44 +0800

    managed to drag and drop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ const companies = [
+   {company_name: 'Zendesk',
+    position: 'Full-stack web developer'
+},
+{
+  company_name: 'Ahrefs',
+  position: 'Reasonml developer'
+}
+ ]
+ return (
+   <DragDropContext>
+     <Droppable droppableId="companies">
+       {(provided)=>(
+         <ul className="companies" {...provided.droppableProps} ref={provided.innerRef}>
+         {companies.map(({company_name, position}, index)=>{
+           return (
+             <Draggable key={company_name} draggableId={company_name} index={index}>
+               {(provided)=>(
+                 <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} >
+                 <div className="cards">
+                 <p>company_name</p>
+                 <p>position</p>
+                 </div>
+                 
+               </li>
+               )}
+             </Draggable>
+             
+           )
+         })}
+       </ul>
+       )}
+     
+     </Droppable>
+     
+   </DragDropContext>
+    )
 }
 
 export default App;
