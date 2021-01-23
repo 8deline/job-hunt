a6e3ebbf56f5ace771483e1972e2a1e9b6bb2fc2
wilsontbx <boonxian1993@hotmail.com>
2021-01-23 23:30:15 +0800

    Delete index.js

--- config/index.js
+++
@@ -1,6 +0,0 @@
-
-const devurl = "http://localhost:5000"
-const produrl =  "https://git.heroku.com/delivery-pool-backend.git"
-
-const api = process.env.NODE_ENV=== "development" ? devurl : produrl;
-export default api
