74665a31f23804eb936b106951ff501fa142db54
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2021-01-30 10:26:02 +0800

    going to do the styling for dashboard

--- src/components/Dashboard/DashboardContent.jsx
+++ src/components/Dashboard/DashboardContent.jsx
@@ -4,13 +4,16 @@ import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import clsx from "clsx";
 import MainBoard from "./MainBoard";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import GridListTileBar from "@material-ui/core/GridListTileBar";
 
 const useStyles = makeStyles((theme) => ({
   appBarSpacer: theme.mixins.toolbar,
   content: {
     flexGrow: 1,
 
-    overflow: "auto",
+    // overflow: "auto",
   },
   container: {
     paddingTop: theme.spacing(2),
@@ -22,11 +25,23 @@ const useStyles = makeStyles((theme) => ({
   paper: {
     padding: theme.spacing(2),
     display: "flex",
-    overflow: "auto",
+    // overflow: "auto",
     flexDirection: "column",
   },
   fixedHeight: {
     height: `calc(100vh - 96px)`,
+  },
+  root: {
+    flexWrap: "wrap",
+    overflowX: "scroll",
+    overflowY: "hidden",
+  },
+  gridList: {
+    flexWrap: "nowrap",
+    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+  },
+  card: {
+    flex: "0 0 auto",
   },
 }));
 
@@ -41,7 +56,51 @@ export default function DashboardContent() {
         <Grid container spacing={3}>
           <Grid item xs={12}>
             <Paper className={fixedHeightPaper}>
-              <MainBoard />
+              {/* <div className={classes.root}>
+                <GridList className={classes.gridList} cols={1}>
+                  <GridListTile>
+                    <div>hahaha</div>
+                    <GridListTileBar title="hahhahahahahahaha" />
+                  </GridListTile>
+                  <GridListTile>
+                    <GridListTileBar title="haha" />
+                  </GridListTile>
+                  <GridListTile>
+                    <GridListTileBar title="haha" />
+                  </GridListTile>
+                  <GridListTile>
+                    <GridListTileBar title="haha" />
+                  </GridListTile>
+                </GridList>
+              </div> */}
+              <div className={classes.root}>
+                <div className={classes.gridList}>
+                  <div className={classes.card}>wakakaka</div>
+                  <div className={classes.card}>wakakaka</div>
+                  <div className={classes.card}>wakakaka</div>
+                  <div className={classes.card}>wakakaka</div>
+                  <div className={classes.card}>wakakaka</div>
+                  <div className={classes.card}>wakakaka</div>
+                  <div className={classes.card}>wakakaka</div>
+                  <div className={classes.card}>wakakaka</div>
+                  <div className={classes.card}>wakakaka</div>
+                  <div className={classes.card}>wakakaka</div>
+                  <div className={classes.card}>wakakaka</div>
+                  <div className={classes.card}>wakakaka</div>
+                  <div className={classes.card}>wakakaka</div>
+                  <div className={classes.card}>wakakaka</div>
+                  <div className={classes.card}>wakakaka</div>
+                  <div className={classes.card}>wakakaka</div>
+                  <div className={classes.card}>wakakaka</div>
+                  <div className={classes.card}>wakakaka</div>
+                  <div className={classes.card}>wakakaka</div>
+                  <div className={classes.card}>wakakaka</div>
+                  <div className={classes.card}>wakakaka</div>
+                  <div className={classes.card}>wakakaka</div>
+                </div>
+              </div>
+
+              {/* <MainBoard /> */}
             </Paper>
           </Grid>
         </Grid>
