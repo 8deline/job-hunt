3950c717de6d9ef972c545e4754d1fe4e761fecc
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2021-01-30 12:45:04 +0800

    fix add new column when no results

--- src/components/Dashboard/MainBoard.jsx
+++ src/components/Dashboard/MainBoard.jsx
@@ -3,9 +3,9 @@ import CategoriesColumn from "./categories";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import { useEffect, useState } from "react";
 import backendService from "../../services/backendAPI";
-import Newcolumn from "./newcolumn"
-import axios from 'axios'
-import qs from 'qs'
+import Newcolumn from "./newcolumn";
+import axios from "axios";
+import qs from "qs";
 
 export default function MainBoard() {
   //  const companiesList = {
@@ -74,13 +74,10 @@ export default function MainBoard() {
 
   // const columns = ["column_id_1", "column_id_2", "column_id_3", "column_id_4"];
 
-
   let [newColumn, setNewColumn] = useState(false);
-  let [colns, setColns]= useState(null)
+  let [colns, setColns] = useState(null);
   let [allresult, setAllResult] = useState(null);
-  let [columnList, setColumnList] = useState(null)
-
-
+  let [columnList, setColumnList] = useState(null);
 
   function getCurrentUser() {
     return JSON.parse(localStorage.getItem("user"));
@@ -92,41 +89,36 @@ export default function MainBoard() {
       .then((result) => {
         setAllResult(result.data.allResult);
         // console.log(result.data.allResult)
-      }) 
+      })
 
       .catch((err) => console.log(err));
-    }, [allresult]);
+  }, [allresult]);
 
   //    const columns  = {};
   //    let columnlisting = [];
   //    for (let i=0; i < result.data.allResult.length; i++) {
   //      columns[result.data.allResult[i].jobstatus] = {column_id: result.data.allResult[i].jobstatus, jobs: result.data.allResult[i].joblist}
   //       columnlisting.push(result.data.allResult[i].jobstatus)
-  //     } 
+  //     }
   //    setColns(columns)
   //   setColumnList(columnlisting)
-     
-     
-  //   })   
+
+  //   })
   //     .catch((err) => console.log(err));
   // }, []);
 
-   
+  //instead of implementing frontend logic to just call the render backend
 
-    //instead of implementing frontend logic to just call the render backend     
+  //  const columns  = {};
+  //  let columnlisting = [];
+  //  for (let i=0; i < result.data.allResult.length; i++) {
+  //    columns[result.data.allResult[i].status.jobstatus] = {column_id: result.data.allResult[i].status.jobstatus, jobs: result.data.allResult[i].joblist}
+  //     columnlisting.push(result.data.allResult[i].status.jobstatus)
+  //   }
+  //  setColns(columns)
+  // setColumnList(columnlisting)
 
-    //  const columns  = {};
-    //  let columnlisting = [];
-    //  for (let i=0; i < result.data.allResult.length; i++) {
-    //    columns[result.data.allResult[i].status.jobstatus] = {column_id: result.data.allResult[i].status.jobstatus, jobs: result.data.allResult[i].joblist}
-    //     columnlisting.push(result.data.allResult[i].status.jobstatus)
-    //   } 
-    //  setColns(columns)
-    // setColumnList(columnlisting)
-     
-     
-     
-        // setAllResult(result.data.allResult);
+  // setAllResult(result.data.allResult);
   //       console.log(result.data.allResult);
   //       const columns = {};
   //       let columnlisting = [];
@@ -140,8 +132,6 @@ export default function MainBoard() {
   //       setColns(columns);
   //       setColumnList(columnlisting);
   //     })
-     
-
 
   const dragEnd = (result) => {
     const { source, destination, draggableId, type } = result;
@@ -156,116 +146,113 @@ export default function MainBoard() {
     ) {
       return;
     }
-    if (type==='company') {
+    if (type === "company") {
       // backendService.updateJob(true, draggableId, destination.droppableId, destination.index)
-    backendService
-    .dragJob(
-      draggableId,
-      source.droppableId,
-      source.index,
-      destination.droppableId,
-      destination.index
-    )
-    .then(result=> {
-      backendService.render(getCurrentUser().email)
-      .then(newresult=> 
-        
-        {
-           setAllResult(newresult)
-  
+      backendService
+        .dragJob(
+          draggableId,
+          source.droppableId,
+          source.index,
+          destination.droppableId,
+          destination.index
+        )
+        .then((result) => {
+          backendService
+            .render(getCurrentUser().email)
+            .then((newresult) => {
+              setAllResult(newresult);
+            })
+            .catch((err) => console.log(err));
+        });
+    } else {
+      axios
+        .post(
+          "http://localhost:5000/api/v1/drag/status",
+          qs.stringify({
+            statusid: allresult[source.index]["_id"],
+            oldorder: source.index,
+            neworder: destination.index,
+          })
+        )
+        .then((result) => {
+          backendService
+            .render(getCurrentUser().email)
+            .then((newresult) => {
+              setAllResult(newresult);
+            })
+            .catch((err) => console.log(err));
         })
-      .catch(err=> console.log(err))
-    })
-
-   
-  }
-
-  else { 
-      axios.post("http://localhost:5000/api/v1/drag/status", qs.stringify({statusid: allresult[source.index]['_id'], oldorder: source.index , neworder: destination.index}))
-      .then(result=>{
-        backendService.render(getCurrentUser().email)
-        .then(newresult=>{
-          setAllResult(newresult)
-        })
-        .catch(err=> console.log(err))
-      })
-      .catch(err=>console.log(err))
-  }
-
+        .catch((err) => console.log(err));
     }
-    
-    // .then(result=>{
-      
-    //   const oldcolnewJobList = Array.from(colns[source.droppableId].jobs);
-    //   let newcolnewJobList =  Array.from(colns[destination.droppableId].jobs);
-    //   const dragItem =  oldcolnewJobList.splice(source.index, 1);
+  };
 
-  
-    //   if (source.droppableId === destination.droppableId) {
-    //      newcolnewJobList = oldcolnewJobList;
-    //    }
-  
-    // newcolnewJobList.splice(destination.index, 0, dragItem[0]);
-  
-    //   const newoldColumn = {
-    //     ...colns[source.droppableId],
-    //     jobs: oldcolnewJobList,
-    //   };
-    //   const newnewColumn = {
-    //     ...colns[destination.droppableId],
-    //     jobs: newcolnewJobList,
-    //   };
-    //   setColns((prev) => ({
-    //     ...prev,
-    //     [source.droppableId]: newoldColumn,
-    //     [destination.droppableId]: newnewColumn,
-    //   }));
-  
-    // })
-    // .catch(err=> console.log(err))
-   
-  
- 
-    
-    // backendService
-    //   .dragJob(
-    //     draggableId,
-    //     source.droppableId,
-    //     source.index,
-    //     destination.droppableId,
-    //     destination.index
-    //   )
+  // .then(result=>{
 
-    //   .then((result) => {
-    //     console.log(result);
-    //   })
-    //   .then((result) => {
-    //     const oldcolnewJobList = Array.from(colns[source.droppableId].jobs);
-    //     let newcolnewJobList = Array.from(colns[destination.droppableId].jobs);
-    //     const dragItem = oldcolnewJobList.splice(source.index, 1);
+  //   const oldcolnewJobList = Array.from(colns[source.droppableId].jobs);
+  //   let newcolnewJobList =  Array.from(colns[destination.droppableId].jobs);
+  //   const dragItem =  oldcolnewJobList.splice(source.index, 1);
 
-    //     if (source.droppableId === destination.droppableId) {
-    //       newcolnewJobList = oldcolnewJobList;
-    //     }
+  //   if (source.droppableId === destination.droppableId) {
+  //      newcolnewJobList = oldcolnewJobList;
+  //    }
 
-    //     newcolnewJobList.splice(destination.index, 0, dragItem[0]);
+  // newcolnewJobList.splice(destination.index, 0, dragItem[0]);
 
-    //     const newoldColumn = {
-    //       ...colns[source.droppableId],
-    //       jobs: oldcolnewJobList,
-    //     };
-    //     const newnewColumn = {
-    //       ...colns[destination.droppableId],
-    //       jobs: newcolnewJobList,
-    //     };
-    //     setColns((prev) => ({
-    //       ...prev,
-    //       [source.droppableId]: newoldColumn,
-    //       [destination.droppableId]: newnewColumn,
-    //     }));
-    //   })
-    //   .catch((err) => console.log(err));
-  
+  //   const newoldColumn = {
+  //     ...colns[source.droppableId],
+  //     jobs: oldcolnewJobList,
+  //   };
+  //   const newnewColumn = {
+  //     ...colns[destination.droppableId],
+  //     jobs: newcolnewJobList,
+  //   };
+  //   setColns((prev) => ({
+  //     ...prev,
+  //     [source.droppableId]: newoldColumn,
+  //     [destination.droppableId]: newnewColumn,
+  //   }));
+
+  // })
+  // .catch(err=> console.log(err))
+
+  // backendService
+  //   .dragJob(
+  //     draggableId,
+  //     source.droppableId,
+  //     source.index,
+  //     destination.droppableId,
+  //     destination.index
+  //   )
+
+  //   .then((result) => {
+  //     console.log(result);
+  //   })
+  //   .then((result) => {
+  //     const oldcolnewJobList = Array.from(colns[source.droppableId].jobs);
+  //     let newcolnewJobList = Array.from(colns[destination.droppableId].jobs);
+  //     const dragItem = oldcolnewJobList.splice(source.index, 1);
+
+  //     if (source.droppableId === destination.droppableId) {
+  //       newcolnewJobList = oldcolnewJobList;
+  //     }
+
+  //     newcolnewJobList.splice(destination.index, 0, dragItem[0]);
+
+  //     const newoldColumn = {
+  //       ...colns[source.droppableId],
+  //       jobs: oldcolnewJobList,
+  //     };
+  //     const newnewColumn = {
+  //       ...colns[destination.droppableId],
+  //       jobs: newcolnewJobList,
+  //     };
+  //     setColns((prev) => ({
+  //       ...prev,
+  //       [source.droppableId]: newoldColumn,
+  //       [destination.droppableId]: newnewColumn,
+  //     }));
+  //   })
+  //   .catch((err) => console.log(err));
 
   if (!allresult) {
     return null;
@@ -274,55 +261,69 @@ export default function MainBoard() {
   return (
     <DragDropContext onDragEnd={dragEnd}>
       <div className="entire-entire">
-      <Droppable droppableId="all-columns" type="columns" direction = "horizontal">
-      {(provided) => (
-      <div className="entire-container" ref={provided.innerRef} {...provided.droppableProps}>
-        {Array.isArray(allresult) && allresult.length !== 0 ? (<> {allresult.map((column, index) => {
-          return (
-            <Draggable draggableId={column.jobstatus} index={index}>
-       {(provided)=>(
-            <div key={column.jobstatus} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} className="job-column">
-              <CategoriesColumn
-                dropid={column.jobstatus}
-                title={column.jobstatus}
-                companies={column.joblist}
-                index={index}
-                setColns={setColns}
-                setColumnList={setColumnList}
-                colns={colns}
-                getCurrentUser={getCurrentUser}
-                setAllResult={setAllResult}
-                allresult={allresult}
-              />
-            
+        <Droppable
+          droppableId="all-columns"
+          type="columns"
+          direction="horizontal"
+        >
+          {(provided) => (
+            <div
+              className="entire-container"
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+            >
+              {Array.isArray(allresult) && allresult.length !== 0 ? (
+                <>
+                  {" "}
+                  {allresult.map((column, index) => {
+                    return (
+                      <Draggable draggableId={column.jobstatus} index={index}>
+                        {(provided) => (
+                          <div
+                            key={column.jobstatus}
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                            className="job-column"
+                          >
+                            <CategoriesColumn
+                              dropid={column.jobstatus}
+                              title={column.jobstatus}
+                              companies={column.joblist}
+                              index={index}
+                              setColns={setColns}
+                              setColumnList={setColumnList}
+                              colns={colns}
+                              getCurrentUser={getCurrentUser}
+                              setAllResult={setAllResult}
+                              allresult={allresult}
+                            />
+                          </div>
+                        )}
+                      </Draggable>
+                    );
+                  })}
+                </>
+              ) : (
+                ""
+              )}
+              {newColumn ? (
+                <Newcolumn
+                  setAllResult={setAllResult}
+                  allresult={allresult}
+                  getCurrentUser={getCurrentUser}
+                  setNewColumn={setNewColumn}
+                />
+              ) : (
+                ""
+              )}
+              {provided.placeholder}
             </div>
-            
-         )}
-         </Draggable>
-          )
-        }) }
-                  
-       
-      {newColumn? <Newcolumn setAllResult={setAllResult} allresult={allresult} getCurrentUser={getCurrentUser} setNewColumn={setNewColumn} /> : ""}
-        </>
-         
-        
-        )
+          )}
+        </Droppable>
 
-        
-      
-         : "" }
-         
-      {provided.placeholder}
-      
+        <button onClick={() => setNewColumn(true)}>Add new column</button>
       </div>
-      
-      )}
-      
-      </Droppable>
-      
-      <button onClick={()=>setNewColumn(true)}>Add new column</button>
-      </div>    
     </DragDropContext>
   );
 }
